03b2f693a3f34f01ec1029d073de7bc11aa8c9e0
Ingy dot Net <ingy@ingy.net>
2010-09-18 00:48:12 -0700

    first commit.

---
+++ lib/App/AYCABTU.pm
@@ -0,0 +1,44 @@
+package App::AYCABTU;
+use 5.008003;
+
+our $VERSION = '0.10';
+use YAML::XS;
+
+sub run {
+}
+
+1;
+
+=encoding utf8
+
+=head1 NAME
+
+App::AYCABTU - All Your Codes Are Belong To Us
+
+=head1 SYNOPSIS
+
+    > aycabtu
+
+=head1 DESCRIPTION
+
+This module installs a program called C<aycabtu>, that can be used to
+manage all of the code repositories that you are interested in.
+
+=head1 RESOURCES
+
+GitHub: L<http://github.com/ingydotnet/app-aycabtu-pm>
+
+=head1 AUTHOR
+
+Ingy döt Net <ingy@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2010. Ingy döt Net
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+See L<http://www.perl.com/perl/misc/Artistic.html>
+
+=cut
